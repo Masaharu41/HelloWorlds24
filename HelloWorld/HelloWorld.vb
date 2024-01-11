2ad193afa67eb4abef5eb9f26279ecdd8ed8cccf
unknown <dalekrebel@gmail.com>
2024-01-11 08:34:39 -0700

    Fixed Application & Added Addition

--- HelloWorld/HelloWorld.vb
+++ HelloWorld/HelloWorld.vb
@@ -4,10 +4,12 @@
 'HelloWorld
 'https://github.com/Masaharu41/HelloWorlds24.git
 
-Friend Module HelloWorld
+Module HelloWorld
     Public Sub Main()
-        Console.WriteLine("HelloWorld2")
-
+        Console.WriteLine("HelloWorld24")
+        Console.Write("5 + 5 = ")
+        Console.Write(5 + 5)
+        Console.WriteLine()
         Console.Read()
     End Sub
 
